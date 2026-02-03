6728565d6c1e7d11e505baa714a1e75d14efd26c
valentin-orrit <vorrit@gmail.com>
2026-02-03 15:27:27 +0100

    setup: update API request test

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,46 @@ import axios from "axios"
 import { useState } from "react"
 
 function App() {
+    const API_URL = import.meta.env.VITE_API_URL || "/api"
+
     const [loading, setLoading] = useState<boolean>(false)
+    const [error, setError] = useState<string | null>(null)
 
-    const testRequest = async (): Promise<JSON> => {
+    const testRequest = async (): Promise<JSON | void> => {
         setLoading(true)
+        setError(null)
 
-        const response = await axios({
-            method: "GET",
-            url: `http://localhost:${import.meta.env.VITE_API_PORT}/api/artist`,
-        })
-        console.log(response)
-        setLoading(false)
-        return response.data
+        const timeoutId = setTimeout(async () => {
+            setLoading(false)
+            setError("Request Timeout")
+        }, 10000)
+
+        try {
+            const response = await axios({
+                method: "GET",
+                url: `${API_URL}/artist`,
+                timeout: 10000,
+            })
+            clearTimeout(timeoutId)
+            console.log(response)
+            setLoading(false)
+            return response.data
+        } catch (error) {
+            clearTimeout(timeoutId)
+            setLoading(false)
+
+            setError(`an error occurred : ${error}`)
+        }
     }
 
     return (
         <>
             <h1>Get a Record</h1>
             <div className="card">
-                <button onClick={testRequest}>test</button>
-                {loading && <div>Loading...</div>}
+                <button onClick={testRequest} disabled={loading}>
+                    {loading ? "Loading..." : "test"}
+                </button>
+                {error && <p>{error}</p>}
             </div>
         </>
     )
